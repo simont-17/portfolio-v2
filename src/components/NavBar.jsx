b9841907cf18f153dfc202902a3126b883f340bf
Simon Truong <78322463+simont-17@users.noreply.github.com>
2024-12-24 20:20:51 -0700

    started boiler plate for new portfolio site

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,17 @@
+import '../styling/NavBar.css'
+
+export default function NavBar() {
+  return (
+    <>
+    
+      <nav className="nav-bar">
+        <div className="nav-fn"><p>SIMON TRUONG</p></div>
+        <div className="nav-tabs">
+          <a>about me</a>
+          <a>skills</a>
+          <a>projects</a>
+        </div>
+      </nav>
+    </>
+  );
+}
