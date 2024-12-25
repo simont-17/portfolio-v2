3b1af6a5dd002ba8d4f0eb76de7229d7a5461fac
Simon Truong <78322463+simont-17@users.noreply.github.com>
2024-12-25 16:24:26 -0700

    switched to hash router for github page compatability

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,22 @@
-import { useState } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 
 import "./App.css";
 import NavBar from "./components/NavBar";
 import AboutMe from "./components/AboutMe";
 import Projects from "./components/Projects";
 
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
-    <Router basename="/portfolio-v2">
+    <Router>
       <NavBar />
       <Routes>
-        <Route path="/" element={<AboutMe />} /> {}
+        <Route path="/" element={<AboutMe />} />
         <Route path="/about-me" element={<AboutMe />} />
         <Route path="/projects" element={<Projects />} />
       </Routes>
     </Router>
   );
 }
-
 export default App;
