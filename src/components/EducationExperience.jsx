3d9bab22e9f0652de6e89384534391075a559788
Simon Truong <78322463+simont-17@users.noreply.github.com>
2024-12-25 00:40:56 -0700

    styling and added information to the tabs

--- src/components/EducationExperience.jsx
+++ src/components/EducationExperience.jsx
@@ -1,6 +1,5 @@
-import * as React from 'react';
-import * as Tabs from '@radix-ui/react-tabs';
-
+import * as React from "react";
+import * as Tabs from "@radix-ui/react-tabs";
 
 export default function EducationExperience() {
   return (
@@ -17,10 +16,39 @@ export default function EducationExperience() {
 
       {/* Tab Content */}
       <Tabs.Content value="tab1" className="tab-content">
-        <div>Content for Tab 1</div>
+        <p className="subtext">Apr 2023 - May 2023</p>
+        <p className="job-title">
+          <b>360 Engineering & Environmental Consulting</b>
+        </p>
+        <p className="subtext">Digital Integration Co-Op Student</p>
+        <ul>
+          <li>
+            Spearheaded the successful migration of over 100 user profiles to
+            Microsoft’s Azure Active Directory from an on-premise domain using
+            Windows 2012, improving security protocols and ensuring regulatory
+            compliance.{" "}
+          </li>
+          <li>
+            Automated client file standardization by developing and deploying
+            custom PowerShell scripts, leading to an increase in workflow
+            efficiency and reducing human error.{" "}
+          </li>
+          <li>
+            Played a pivotal role in the formation of the IT department,
+            contributing to long-term infrastructure strategies that set the
+            foundation for scalable future growth.
+          </li>
+        </ul>
       </Tabs.Content>
       <Tabs.Content value="tab2" className="tab-content">
-        <div>Content for Tab 2</div>
+        <p className="subtext">Sept 2020 - Dec 2024</p>
+        <p className="school-name">
+          <b>Mount Royal University</b>
+        </p>
+        <p className="subtext degree-name">Bachelor of Computer Information Systems</p>
+        <ul className="bullet-list">
+            <li>Dean's Honour Roll x3</li>
+        </ul>
       </Tabs.Content>
     </Tabs.Root>
   );
